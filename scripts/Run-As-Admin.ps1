1497404b92e5daa750924ef954d890d5c45de320
Saurabh Kumar Sharma <imesskayesss@gmail.com>
2023-01-03 06:17:11 +0530

    accepting varargs

--- scripts/Run-As-Admin.ps1
+++ scripts/Run-As-Admin.ps1
@@ -1,8 +1,9 @@
 # runs the passed commands as admin
 function Run-As-Admin {
     param(
-       [String]$ArgList
+       [String[]]$ArgList
     )
+    $ArgList = $ArgList -join " "
     Write-Host "attempting to run powershell with `"$ArgList`""
     Start-Process powershell -ArgumentList "-Command `" . ${HOME}\${PROFILE}; $ArgList`"" -verb runAs
 }
